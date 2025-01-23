d4229d6ab77c6bd1b386eb4a3674346d1963285b
koskedk <koskedk@live.com>
2025-01-23 13:50:43 +0300

    initial dev

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from loguru import logger
+
+from startup import add_logging
+
+add_logging()
+
+logger.info("starting livesync.ship...")
+app = FastAPI(title="livesync.ship")
+logger.error("NOT LICENSE FOUND !")
+logger.info("livesync.ship started !")
+
+
+@app.get("/")
+async def index():
+    logger.info("xxxx")
+    return {"status": "ship is alive"}
